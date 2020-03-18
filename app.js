09db97111a7b08a4b2528cd6ef82d6706893769a
In Seop Hwang <inseop@Ins-MacBook-Pro.local>
2020-03-18 11:22:42 -0400

    added mongo / session

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let MongoStore = require('connect-mongo')(session)
+// require('./lib/passport')
+require('dotenv').config()
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('MongoDB Connected!')
+})
+.catch(err => console.log(`MongoDB err: ${err}`))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +34,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET, 
+    store: new MongoStore({
+        url: process.env.MONGODB_URI,
+        autoReconnect: true,
+        cookie: {maxAge: 60000}
+    })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req, res, next) => {
+    res.locals.user = req.user
+    next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
